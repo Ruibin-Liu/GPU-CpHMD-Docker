2b856cb1725905b530181e63ce00846e4524b6b9
Ruibin Liu <rainbee1988@gmail.com>
2020-06-30 22:01:04 -0400

    PATH

--- cuda10_install.sh
+++ cuda10_install.sh
@@ -8,3 +8,10 @@ sudo apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda
 sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
 sudo apt update
 sudo apt install cuda-10-0   # or other versions like 10.1 and 10.2
+cat << EOF >> ~/.bashrc
+# set PATH for cuda 10.0 installation
+if [ -d "/usr/local/cuda-10.0/bin/" ]; then
+    export PATH=/usr/local/cuda-10.0/bin${PATH:+:${PATH}}
+    export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+fi
+EOF
