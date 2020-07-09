9be0deb7793a34b65107c30d83b3cc7ebf5bbbba
Ruibin Liu <ruibinliuphd@gmail.com>
2020-07-09 15:15:41 -0400

    Move the 'usermod' command to the end
    If the command is not at the end, its following commands won't be exeucted.

--- docker_installation.sh
+++ docker_installation.sh
@@ -1,23 +1,24 @@
 DIST=$(lsb_release -c |awk '{print $NF}')
-# For docker
+# for docker and awscli
 sudo apt update
 sudo apt install -y python-pip apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $DIST stable" 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $DIST stable"
 sudo apt update
 sudo apt install -y docker-ce
-sudo usermod -aG docker $USER  && newgrp docker # add user to the docker grou
+sudo pip install docker-compose
+sudo pip install awscli
 
-# For nvidia-docker
+# for nvidia-docker
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-docker2
-sudo systemctl restart docker
-pip install docker-compose
 
 # change /etc/docker/daemon.json
 sudo cp /etc/docker/daemon.json /etc/docker/backup_daemon.json
 sudo cp sample_daemon.json /etc/docker/daemon.json
 echo 'The original /etc/docker/daemon.json file is backed up as /etc/docker/backup_daemon.json'
-sudo pkill -SIGHUP dockerd
+
+# add user to the docker group
+sudo usermod -aG docker $USER  && newgrp - 
