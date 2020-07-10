da56b9a0d152509b2b3c264ef8e7ddde9a3b1e9a
Ruibin Liu <ruibinliuphd@gmail.com>
2020-07-10 10:46:02 -0400

    update

--- install_nvidia_docker.sh
+++ install_nvidia_docker.sh
@@ -1,6 +1,6 @@
 sudo systemctl restart docker
 sudo systemctl enable docker
-echo "Docker installed!"
+echo "Docker Detected!"
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-container-runtime
 sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo tee /etc/systemd/system/docker.service.d/override.conf <<EOF
@@ -10,15 +10,7 @@ ExecStart=/usr/bin/dockerd --host=fd:// --add-runtime=nvidia=/usr/bin/nvidia-con
 EOF
 sudo systemctl daemon-reload
 sudo systemctl restart docker
-sudo tee /etc/docker/daemon.json <<EOF
-{
-    "runtimes": {
-        "nvidia": {
-            "path": "/usr/bin/nvidia-container-runtime",
-            "runtimeArgs": []
-        }
-    }
-}
-EOF
+sudo cp sample_daemon.json /etc/docker/daemon.json 
 sudo pkill -SIGHUP dockerd
-echo "Nvidia-docker installed"
+echo "Nvidia-docker installed!"
+sudo -H pip install awscli docker-compose
