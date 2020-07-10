8ccda622978eb0fe442952a821f7060009451170
Ruibin Liu <rainbee1988@gmail.com>
2020-07-10 10:52:00 -0400

    source

--- install_nvidia_docker.sh
+++ install_nvidia_docker.sh
@@ -1,6 +1,9 @@
 sudo systemctl restart docker
 sudo systemctl enable docker
 echo "Docker installed!"
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-container-runtime
 sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo tee /etc/systemd/system/docker.service.d/override.conf <<EOF
